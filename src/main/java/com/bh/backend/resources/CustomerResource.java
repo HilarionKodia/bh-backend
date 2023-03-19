0a6a0b039d4571d97ee66b611409ccfd5c004d48
Hilarion KODIA <HilarionKodia@Gmail.com>
2023-03-19 17:15:42 +0100

    Init Customer Ressource

--- src/main/java/com/bh/backend/resources/CustomerResource.java
+++ src/main/java/com/bh/backend/resources/CustomerResource.java
@@ -1,24 +1,50 @@
 package com.bh.backend.resources;
 
+import com.bh.backend.mappers.CustomerMapper;
+import com.bh.backend.models.CustomerDTO;
 import com.bh.backend.services.CustomerService;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.logging.Logger;
 
-@Path("/hello2")
+@Path("/customer")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Transactional
+@ApplicationScoped
 public class CustomerResource {
+
+    private static final Logger LOGGER = Logger.getLogger(CustomerResource.class.getName());
 
     @Inject
     CustomerService customerService;
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello2() {
+    @Inject
+    CustomerMapper customerMapper;
 
-        customerService.get();
-        return "Hello from RESTEasy Reactive";
+    @GET
+    @Path("")
+    public Response getAllCustomers() {
+        try{
+            List<CustomerDTO> customers= customerMapper.toCustomerDTOList(customerService.get());
+            if (customers.isEmpty()) {
+                return Response.noContent().build();
+            } else {
+                return Response.ok(customers).build();
+            }
+        } catch (Exception e) {
+            LOGGER.severe(e.getMessage());
+            return Response.serverError().build();
+        }
+
     }
 }
