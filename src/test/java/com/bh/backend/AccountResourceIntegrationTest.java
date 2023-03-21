1ae292a93df24a1166b189f1c84ddc68c518eaa5
Hilarion KODIA <HilarionKodia@Gmail.com>
2023-03-21 11:59:43 +0100

    Fix Account Resource Integration Test

--- src/test/java/com/bh/backend/AccountResourceIntegrationTest.java
+++ src/test/java/com/bh/backend/AccountResourceIntegrationTest.java
@@ -4,13 +4,15 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.core.Is.is;
 
 @QuarkusTest
 public class AccountResourceIntegrationTest {
 
     @Test
-    void shouldOpenAnAccountWithBalance0() {
+    void shouldOpenAnAccountWithBalance0AndNotSendATransactionOnTheAccount() {
         given().body("{\"customerID\": 1, \"initialCredit\": 0}")
                 .header("Content-Type", "application/json")
                 .when()
@@ -18,12 +20,13 @@ public class AccountResourceIntegrationTest {
                 .then()
                 .statusCode(200)
                 .body("size()", is(3),
-                        "accountId", is(1),
-                        "balance", is(0.0F));
+                        "accountId", notNullValue(),
+                        "balance", is(0.0F),
+                        "transactions", nullValue());
     }
 
     @Test
-    void shouldOpenAnAccountWithBalance4AndSendATransactionOnTheAccount() {
+    void shouldOpenAnAccountWithBalance4AndSendOneTransactionOnTheAccount() {
         given().body("{\"customerID\": 1, \"initialCredit\": 4}")
                 .header("Content-Type", "application/json")
                 .when()
@@ -31,11 +34,12 @@ public class AccountResourceIntegrationTest {
                 .then()
                 .statusCode(200)
                 .body("size()", is(3),
-                        "accountId", is(1),
+                        "accountId", notNullValue(),
                         "balance", is(4.0F),
+                        "transactions", notNullValue(),
                         "transactions.size()", is(1),
                         "transactions[0].transactionId", is(1),
-                        "transactions[0].accountId", is(1),
+                        "transactions[0].accountId", notNullValue(),
                         "transactions[0].amount", is(4.0F));
     }
 }
